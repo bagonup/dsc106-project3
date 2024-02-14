db9a90a308ab530612cfbde43a3c064f5bda0052
David Yonemura <dyonemura@ucsd.edu>
2024-02-14 12:19:13 -0800

    Added code for importing the data into the website

---
+++ src/components/App.svelte
@@ -0,0 +1,20 @@
+<script>
+//Imports
+import { onMount } from 'svelte';
+import * as d3 from 'd3';
+
+// Load Data
+let tempData = [];
+
+onMount(async () => {
+
+    const res = await fetch('owid-energy-data.csv'); 
+
+    const csv = await res.text();
+
+    tempData = d3.csvParse(csv, d3.autoType)
+
+    console.log(tempData);
+
+});
+</script>
