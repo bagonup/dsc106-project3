22bb1958d9621330c1c3d440ae4bfce09aef5cbd
Bryan Gonzalez <bagonzal@ucsd.edu>
2024-02-16 13:42:12 -0800

    added new data with total consumption and by continent

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -1,35 +1,34 @@
 <script>
-    
-//Imports
-import { onMount } from 'svelte';
-import * as d3 from 'd3';
+  //Imports
+  import { onMount } from "svelte";
+  import * as d3 from "d3";
 
-// Load Data
-let tempData = [];
+  // Load Data
+  let tempData = [];
 
-onMount(async () => {
-
-    const res = await fetch('truncated_energy_data.csv'); 
+  onMount(async () => {
+    const res = await fetch("data_energy.csv");
 
     const csv = await res.text();
 
-    tempData = d3.csvParse(csv, d3.autoType)
+    tempData = d3.csvParse(csv, d3.autoType);
 
     console.log(tempData);
-
-});
+  });
 </script>
 
 <!--Website Code-->
 
 <main>
-    <h1>The World in Energy</h1>
-    <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-
+  <h1>The World in Energy</h1>
+  <p>
+    Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+  </p>
 </main>
 
 <!--Styling for Website. Fonts and other things-->
 <style>
-    h1 {text-align: center;}
-
+  h1 {
+    text-align: center;
+  }
 </style>
