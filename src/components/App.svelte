c139875cb1055b6243283dc9f37d8d17e5959c8e
David Yonemura <dyonemura@ucsd.edu>
2024-02-18 15:23:45 -0800

    pie chart code

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -3,8 +3,12 @@
   import { onMount } from "svelte";
   import * as d3 from "d3";
 
-  // Load Data
+  // Load tempData
   let tempData = [];
+  let svg;
+  let fixdata;
+  let groupedData;
+  let summedData;
 
   onMount(async () => {
     const res = await fetch("data_energy.csv");
@@ -14,6 +18,59 @@
     tempData = d3.csvParse(csv, d3.autoType);
 
     console.log(tempData);
+
+    fixdata = tempData.filter(function(d) { return d.Continent != null && d.total_consumption != null;});
+    console.log(fixdata);
+
+    groupedData = d3.group(fixdata, d => d.Continent);
+
+    summedData = Array.from(d3.rollup(fixdata, v => d3.sum(v, d => d.total_consumption), d => d.Continent));
+    let dataWithNamedProperties = summedData.map(([Continent, total_consumption]) => ({Continent, total_consumption}));
+
+    console.log(dataWithNamedProperties);
+
+    // Assuming data is an array of objects with 'country' and 'consumption' properties
+
+var width = 1000;
+var height = 1000;
+var radius = Math.min(width, height) / 2;
+
+var svg = d3.select('body')
+  .append('svg')
+  .attr('width', width)
+  .attr('height', height)
+  .append('g')
+  .attr('transform', 'translate(' + width / 2 + ',' + height / 2 + ')');
+
+var color = d3.scaleOrdinal(d3.schemeCategory10);
+
+var pie = d3.pie()
+  .value(function(d) { return d.total_consumption; })
+  .sort(null);
+
+var path = d3.arc()
+  .outerRadius(radius - 10)
+  .innerRadius(0);
+
+var label = d3.arc()
+  .outerRadius(radius - 40)
+  .innerRadius(radius - 40);
+
+var g = svg.selectAll('.arc')
+  .data(pie(dataWithNamedProperties))
+  .enter().append('g')
+  .attr('class', 'arc');
+
+g.append('path')
+  .attr('d', path)
+  .style('fill', function(d) { return color(d.data.Continent); });
+
+g.append('text')
+  .attr('transform', function(d) { return 'translate(' + label.centroid(d) + ')'; })
+  .attr('dy', '0.35em')
+  .text(function(d) { return d.data.Continent; });
+
+
   });
 </script>
 
